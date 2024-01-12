dfc22d971afe946392244e7bd9c692041f503da6
Richard Risner <78838432+rrisner@users.noreply.github.com>
2024-01-11 22:34:19 -0500

    Remove unnecessary comments and add a p to about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,6 +6,7 @@ function About() {
       
       <h1>About Richard</h1>
       <p>Richard has a bachelor&apos;s degree from Indiana University Southeast.</p>
+      <p>Richard has also taken several technical and business classes at Wake Technical Community College.</p>
       <p className="fun-class">
         Richard is high class!
       </p>
